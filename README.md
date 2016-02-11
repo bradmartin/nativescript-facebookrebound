3ea2605bc1b0824eea40b9ca614281b987f4d9fe
bradmartin <bradmartin0905@gmail.com>
2016-02-11 17:15:09 -0600

    need to figure out the API and good to go

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# nativescript-facebookrebound
+# nativescript-facebookrebound :basketball:
 NativeScript plugin to use Facebook's Spring Animations for Android
+
+### FacebookRebound Usage 
+<span style="color: #de3a3a">** The .gif doesn't show how smooth this really is. **</span>
+![FacebookRebound](facebookRebound.gif)
+
+## Installation
+`npm install nativescript-facebookrebound`
+
+## Usage
+
+### XML:
+```XML
+ <Image tap="springThis" height="240" loaded="picLoaded" src="~/images/deadpool2.jpg" stretch="aspectFit" />
+```
+
+### JS:
+```JS
+var rebound = require("nativescript-facebookrebound");
+
+// Any tap event in NativeScript has (args) as the passed EventData.
+// args.object is the View/component that triggered the tap event
+function reboundThis(args) {
+    // TODO - figure out the API for this...
+}
+exports.bangThis = bangThis; 
+```
--- demo/app/main-page.js
+++ demo/app/main-page.js
@@ -1,46 +1,42 @@
 var app = require("application");
 var platformModule = require("platform");
 var color = require("color");
-var twitterBang = require("nativescript-twitterbang");
+var Observable = require("data/observable").Observable;
+
+var data = new Observable({});
 
 function pageLoaded(args) {
     var page = args.object; 
     // Change statusbar color on Lollipop
     if (platformModule.device.sdkVersion >= "21") {
         var window = app.android.startActivity.getWindow(); 
-        window.setStatusBarColor(new color.Color("#D50000").android);
-    }   
-}
+        window.setStatusBarColor(new color.Color("#144474").android);
+    }
+    page.bindingContext = data;
+} 
 exports.pageLoaded = pageLoaded;
 
 var viewToAnimate;
-var TENSION = 800;
-var DAMPER = 20;
+var TENSION = 500;
+var DAMPER = 10;
 var mSpring;
+
 
 function picLoaded(args) {
 
     viewToAnimate = args.object.android;
-    // viewToAnimate.setOnTouchListener(app.android.foregroundActivity);
-
 
     var springSystem = com.facebook.rebound.SpringSystem.create();
     mSpring = springSystem.createSpring();
 
     var listener = com.facebook.rebound.SimpleSpringListener.extend({
         onSpringUpdate: function (spring) {
-            // console.log(spring);
             var value = spring.getCurrentValue();
             console.log(value);
             console.log('value: ' + value);
-            // var scale = 1 - (value * 0.001);
-            // console.log('scale: ' + scale);
-
             var mappedValue = com.facebook.rebound.SpringUtil.mapValueFromRangeToRange(mSpring.getCurrentValue(), 0, 1, 1, 0.5);
             viewToAnimate.setScaleX(mappedValue);
             viewToAnimate.setScaleY(mappedValue);
-
-            // viewToAnimate.setY(value);
 
         }
     });
@@ -49,37 +45,18 @@ function picLoaded(args) {
 
     var config = new com.facebook.rebound.SpringConfig(TENSION, DAMPER);
     mSpring.setSpringConfig(config);
-
 }
 exports.picLoaded = picLoaded;
 
 
 var mMovedUp = false;
-var mOrigY = null;
 
 function springThis(args) {
     if (mMovedUp) {
         mSpring.setEndValue(0);
     } else {
-        mOrigY = viewToAnimate.getY();
-
         mSpring.setEndValue(1);
     }
-
     mMovedUp = !mMovedUp;
 }
 exports.springThis = springThis;
-
-
-function bangThis(args) {
-    var view = args.object.android;
-    twitterBang.bang(view);
-}
-exports.bangThis = bangThis;
-
-function bangHeart(args) {
-    var view = args.object.android;
-    twitterBang.bang(view);
-    args.object.src = "~/images/twitterHeart.png";
-}
-exports.bangHeart = bangHeart;
